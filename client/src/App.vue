b3b8073cbbee24753ea0c7c950244a7e2995d70c
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-02-25 13:04:34 -0500

    update Vue app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,141 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      
+      <body> 
+        <header>
+          <h1>Runtime Fitness</h1>
+          <h4>Track and share your fitness journey</h4>
+          <!-- <h4></h4> -->
+        </header>    
+
+        <div class="content">
+
+          <div class="container">
+            <div class="navbar">
+              <router-link tag="a" to="/Home.vue">Home</router-link> 
+              <a href="#contact">Track</a>
+            </div>
+            <div class="dropdown">
+              <router-link tag="a" to="/Discover.vue">Discover</router-link>
+                <div class="dropdown-content">
+                  <ul>  
+                    <li><a href="#">Runtime Workouts</a></li>
+                    <li><a href="#">Community Catalogue</a></li>
+                    <li><a href="#" class="last">Challenge Page</a></li>
+                  </ul>
+                </div>
+            </div>
+            <div class="sign-in" >
+              <button class="sign-in" onclick="openLogin()">Sign In/Sign Up</button>
+            </div>
+
+             <!-- LOGIN  -->
+
+            <div class="login-popup" id="loginForm">
+              <form action="/action_page.php" class="login-container">
+                <button type="button" class="close" onclick="closeLogin()">x</button>
+
+                <h1>Login</h1>
+          
+                <label for="email"><b>Email</b></label>
+                <input class="emailinput" type="text" placeholder="Enter Email" name="email" required>
+                <br>
+                <label class="pswlabel" for="psw"><b>Password</b></label>
+                <input class="pswinput" type="password" placeholder="Enter Password" name="psw" required>
+                <br>
+                <button type="submit" class="loginbtn">Login</button>
+                <br> 
+                <button type="button" class="btn" onclick="signUpForm(); closeLogin()">Not a member? Sign Up</button>
+              </form>
+            </div>
+
+        
+
+                <!-- SIGN UP  -->
+        
+            <div class="signUp-popup" id="signUpForm">
+              <form action="/action_page.php" class="signUp-container">
+
+                <h1>Background Information</h1>
+          
+                <label class="first" for="firstName"><b>First Name</b></label>
+                <input class="firstName" type="text" placeholder="John" name="firstName" required>
+                <br>
+                <label class="last" for="lastName"><b>Last Name</b></label>
+                <input class="lastName" type="text" placeholder="Doe" name="lastName" required>
+                <br>
+                <label class="bday" for="birthday"><b>Date of Birth</b></label>
+                <input class="birthday" type="date" placeholder="01/02/1993" name="birthday" required>
+
+                <h1 class="register">Account Registration</h1>
+
+                <label class="usr" for="username"><b>Username</b></label>
+                <input class="username" type="text" placeholder="Create a Username" name="username" required>
+                <br>
+                <label for="email"><b>Email</b></label>
+                <input class="email" type="text" placeholder="Enter Email" name="email" required>
+                <br>
+                <label class="pswlabel" for="psw"><b>Password</b></label>
+                <input class="password" type="password" placeholder="Enter Password" name="psw" required>
+                <br>
+                <label class="mobile" for="phone"><b>Mobile #</b></label>
+                <input class="phone" type="tel" placeholder="(888)888-8888" name="phone">
+                <br>
+                
+                <div class="checkbox-question1">
+                  <input type="checkbox" name="question1">
+                  <label for="question1">Would you like to receive text notifications?</label>
+                  <br>
+                </div>
+                <div class="checkbox-question2">
+                  <input type="checkbox" name="question2">
+                  <label for="question2">Would you like to receive email notifications?</label>
+                </div>
+
+                <div class="terms-heading">
+                  <h4>Terms & Conditions</h4>
+                </div>
+
+                <div class="terms-container">
+                  <h5>A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. A bunch of terms. </h5>
+                </div>
+                
+                <input type="checkbox" name="terms" required>
+                <label class ="terms-agreement" for="terms">I agree to the Terms & Conditions</label>
+
+                <br>
+                <button type="submit" class="registerbtn" onclick="closeSignUp()">Register</button>
+
+                <button type="button" class="cancel" onclick="closeSignUp()">Cancel</button>
+
+              </form>
+            </div>
+
+        </div>
+       </div>
+      </body>
+         <div>
+           <router-view/>
+        </div>
+          <!-- ************************* FOOTER *********************** -->
+          <footer>
+            <ul> 
+              <li><a href=#>About</a></li>
+              <li><a href=#>Contact</a></li>
+              <li><a href=#>Affiliations</a></li>
+              <li class="year">Copyright 2021</li>
+            </ul>
+          </footer>
+        
+
+      
+    </div>      
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat&display=swap');
 </style>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>This is new code</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <body>
+    <h2>Welcome to Runtime Fitness!</h2>
+        <p>We hope you're ready to take on the fitness journey of a lifetime. Here you can upload your fitness #Goals, #Gains, #Tips and #Journey!<br> There are so many ways to stay motivated including <a href=#>Share with Friends</a>, <a href=#>Challenge Friends</a> and by checking the <a href=#>Local Challenges</a> page.</p>
+      
+        <h2>More Motivation</h2>
+
+        <h3>New to fitness?</h3> 
+        <p>Don't know where to start? Well don't quit while you're ahead! <a href=#>Here</a> you can set up virtual appointments with fitness coaches and nutritionists who can help you develop workout plans and meal plans based on your goals, whatever they may be. </p>
+        <h3>Discover</h3> 
+        <p>Our very own team at Runtime Fitness has filmed over 100 workout videos for you to watch, learn and follow. We have categorized our videos from beginner to advanced and can find them in the <a href=#>Discover</a> section. If your interests are more specific, you can also search through the <a href=#>Community Catalogue</a> where members upload their very own workout videos for the world to see.</p>
+        <h3>Share</h3>
+        <p>For those of you who want to create and share your own fitness content, here at Runtime Fitness, you're able to do so. You can <a href=#>Upload Videos</a> to our Community Catalogue that will also be posted on your account's feed. If you make your profile public, you will be able to attract followers from all around the world who can watch and react to your fitness journey.</p>
+        <p>Please make sure to read our <a href=#>Rules & Regulations</a> documentation before uploading content to the Community Catalogue. You will need to sign off on the document before the app allows you to post photos or videos.</p>
+        <figure class="image">
+          <img src="gym1.jpg"
+                width="700" height="360" alt="Gym equipment">
+        </figure>
+        <img src="gym2.jpg"
+                width="600" height="360" alt="Gym equipment">
+
+    
+    
+        <div class="social">
+          <h3>Sign in on these platforms to share your Runtime content!</h3>
+            <ul>
+              <li><a href="#">Email</a></li>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+
+        <div class="field">
+          <label>Stay up to date!</label><br>
+          <input class="input" type="email" style="height: 30px; width: 200px;" placeholder="Enter your email">
+        </div>
+   
+  </body>
+
+
   </div>
 </template>
 
+<style scoped>
+  .home a {
+    text-decoration: underline;
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
