6c484d97ade3d0a5fc9fec385c4867f50eb54cdf
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-03-05 01:55:06 -0500

    new badges

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="buttons" v-if="!Session.user">
+            <router-link class="navbar-item button is-light" tag="a" to="/signup">
+                Sign Up    
+            </router-link>
+            <router-link class="navbar-item button is-light" tag="a" to="/login">
+                Login     
+            </router-link>
+        </div>
+        <div class="greeting" v-else>
+            Hello, Jasmine
+            (<a @click="logout">
+                Log out
+            </a>)
+        </div>
+    </div>        
+</template>
+
+<script>
+    import Session from "../models/Session"
+export default {
+    data(){
+        return {
+            Session
+        }
+    },
+    methods: {
+        login(){
+            this.Session.user = { name: "Jasmine" }
+        },
+        logout(){
+            this.Session.user = null
+        }
+    }
+}
+</script>
+
+<style>
+.buttons {
+    margin-top: 10px;
+}
+.greeting {
+    font-family: 'Bangers', cursive;
+    margin: 15px 10px;
+    font-size: 25px;
+    color: black;
+}
+.greeting a {
+    color: #ffba08;
+}
+</style>
