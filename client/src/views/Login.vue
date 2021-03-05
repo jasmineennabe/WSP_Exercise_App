10bf3124db0566b53112b0401f0595f3ba6a3af2
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-03-05 01:55:29 -0500

    new views

---
+++ client/src/views/Login.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="login-popup" id="loginForm" v-if="!Session.user">
+        <form action="/action_page.php" class="login-container">
+        <button type="button" class="close" @click='close'>
+            X
+        </button>
+
+        <h1>Login</h1>
+    
+        <label for="email">
+            <b>Email</b>
+        </label>
+        <input class="emailinput" type="text" placeholder="Enter Email" name="email" required>
+        <br>
+        <label class="pswlabel" for="psw">
+            <b>Password</b>
+        </label>
+        <input class="pswinput" type="password" placeholder="Enter Password" name="psw" required>
+        <br>
+        <button type="submit" class="loginbtn" @click='login'>
+            Login
+        </button>
+        <br> 
+        </form>
+    </div>
+</template>
+
+<script>
+    import Session from "../models/Session"
+export default {
+    data(){
+        return {
+            Session
+        }
+    },
+    methods: {
+        login(){
+            this.Session.user = { name: "Jasmine" }
+        },
+        close(){
+            window.history.back()
+        },
+        toMyPage(){
+            this.router.navigate(['/mywall'])
+        }
+    }
+}
+</script>
+
+<style>
+  .login-popup {
+    background-color: white;
+    margin: 100px auto;
+    border: 3px solid #f1f1f1;
+    text-align: center;
+    font-family: 'Montserrat', sans-serif;
+    width: 50vw;
+    color: black;
+  } 
+  .pswlabel b{
+    margin-right: 20px;
+    margin-left: -15px;
+  }
+  .emailinput {
+    margin-left: 28px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: 'Montserrat', sans-serif;
+  }
+  .pswinput {
+    margin-top: 10px;
+    margin-left: -10px;
+    margin-bottom: 20px;
+    font-family: 'Montserrat', sans-serif;
+  }
+  
+  /* Add styles to the form container */
+  .login-container {
+    margin: 10px 0 20px 0;
+    background-color: white;
+  }
+  .login-popup h1 {
+    margin-top: 0;
+    clear: right;
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 35px;
+  }
+  
+  /* Full-width input fields */
+  .login-container input[type=text], .login-container input[type=password] {
+    width: 50%;
+    padding: 15px;
+    border-radius: 5px;
+    border: none;
+    background: #f1f1f1;
+  }
+  
+  /* When the inputs get focus, do something */
+  .login-container input[type=text]:focus, .login-container input[type=password]:focus {
+    background-color: #ddd;
+    outline: none;
+  }
+  button.close {
+    float: right;
+    text-align: right;
+    background-color: white;
+    color: black;
+    padding: 6px;
+    margin-left: 400px;
+    margin-right: 10px;
+    margin-top: 0;
+  }
+  
+  .loginbtn {
+    background-color: #d90429;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 20px;
+    padding: 10px 30px;
+    margin-bottom: 20px;
+    font-family: 'Bebas Neue', sans-serif;
+  }
+  
+  /* Set a style for the submit/login button */
+  .login-container .btn {
+    background-color: #ffba08;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    opacity: 0.8;
+  }
+  
+  /* Add some hover effects to buttons */
+  .login-container .btn:hover, .loginbtn:hover  {
+    background-color: #474747;
+    opacity: .5;
+  }
+  .close:hover {
+    color: black;
+    background-color: rgba(71, 71, 71, .3);
+  }
+
+
+</style>
---
+++ client/src/views/Routine.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="routine">
+    <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Give Your Workout a Name">
+        </div>
+    </div>
+
+    <div class="field smaller">
+        <label class="label">Date</label>
+        <div class="control">
+            <input class="input smaller" type="date">
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Exercises</label>
+        <div class="control">
+            <table class="table is-bordered" >
+                <thead>
+                    <tr>
+                        <th class="th-e">Exercise</th>
+                        <th>Reps</th>
+                        <th>Sets</th>
+                        <th>Weight</th>
+                        <th>Notes</th>
+                        <th><button @click="newRow">+</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, i) in rowData" :key='i'>
+                        <th scope="row">{{ item.exercise }}</th>
+                        <td>{{ item.reps }}</td>
+                        <td>{{ item.sets }}</td>
+                        <td>{{ item.weight }}</td>
+                        <td>{{ item.notes }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="toAdd">Add new row</div>
+
+    <div class="field is-grouped">
+        <div class="control">
+            <button type="submit" class="button">Finish</button>
+        </div>
+        <div class="control">
+            <!-- <button @click="">Cancel</button> -->
+            <router-link to="/track" tag="a">Cancel</router-link>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data: ()=> ({
+        exercise: '',
+        reps: '',
+        sets: '',
+        weight: '',
+        notes: '',
+        rowData: []
+    }),
+    methods: {
+        newRow() {
+            var myTable = {
+                exercise:this.exercise,
+                reps:this.reps,
+                sets:this.sets,
+                weight:this.weight,
+                notes:this.notes
+            };
+            this.rowData.push(myTable) 
+            this.exercise = '';
+            this.reps = '';
+            this.sets = '';
+            this.weight = '';
+            this.notes = '';
+        },
+
+    }  
+}
+</script>
+
+<style>
+.routine {
+    width: 50vw;
+    margin: 20px auto;
+    border: solid;
+    border-color: #c5c5c5;
+    border-radius: 4px;
+    padding: 20px;
+    font-family: 'Montserrat', sans-serif;
+}
+.field label.label {
+    font-size: 25px;
+    font-family: 'Bebas Neue', sans-serif;
+    margin-bottom: 0;
+}
+.field {
+    display: inline-block;
+    margin: 0 20px;
+    width: 350px;
+    height: 95px;
+}
+.input {
+    width: 400px;
+}
+.smaller {
+    width: 200px;
+}
+button.button {
+    background-color: #ffba08;
+    color: white;
+    border-radius: 4px;
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 22px;
+}
+
+.is-grouped a {
+    color: black;
+    text-decoration: underline;
+    font-family: 'Bebas Neue', sans-serif;
+    padding: 10px 22px 10px 22px;
+    font-size: 22px;
+} 
+.table button {
+    background-color: white;
+    border: none;
+    color: #710000;
+    font-size: 20px;
+    padding: 0;
+    font-weight: bolder;
+    cursor: pointer;
+}
+th.th-3 {
+    padding: 8px 50px;
+}
+div.toAdd {
+    visibility: hidden;
+    font-family: 'Montserrat', sans-serif;
+    background-color: rgba(113, 0, 0, .8);
+    color: white;
+    width: 60px;
+    font-size: 16px;
+    font-weight: bolder;
+    margin: 0;
+    padding: 0 3px;
+}
+button:hover div.toAdd {
+    display: inline;
+    visibility: visible;
+}
+
+</style>
