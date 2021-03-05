8ce04e300bc16b19347d350b52b09a21e7ea0d16
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-03-05 01:54:23 -0500

    importing component

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import FriendsSmall from './components/FriendsSmall'
 
 import 'bulma';
 import 'buefy/dist/buefy.css'
@@ -8,6 +9,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import './sass/main.scss'
 import 'chart.js'
 
+Vue.component('FriendsSmall', FriendsSmall);
 Vue.config.productionTip = false
 
 new Vue({
