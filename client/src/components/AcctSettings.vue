ca5fdc7f7b3652e7e0f588dd5931c329b2a98054
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-03-07 11:04:36 -0500

    acct settings

---
+++ client/src/components/AcctSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="acct-settings">
+      <!-- make them switch when button is pressed -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Settings.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="settings">
+      <div class="columns">
+          <div class="column is-one-quarter">
+              <div class="setting-header">
+                  Settings
+              </div>
+              <div class="settings-list">
+                  <div class="setting-item">
+                      <button>My Account</button>
+                  </div>
+                  <div class="setting-item">
+                      <button>Preferences</button>
+                  </div>
+                  <div class="setting-item">
+                      <button>Support</button>
+                  </div>
+              </div>
+          </div>
+          <div class="column settings-content">
+              
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.settings {
+    margin: 40px;
+}
+.column.settings-content {
+    margin: 0 20px;
+    border: solid;
+    border-color: #c7c7c7;
+    border-radius: 4px;
+    padding: 10px;
+}
+.is-one-quarter {
+    background-color: #e4e4e4;
+    border: solid;
+    margin: 0 20px;
+    border-color: #c7c7c7;
+    border-radius: 4px;
+    padding: 10px;
+}
+.setting-header {
+    font-family: 'Bangers', cursive;
+    font-size: 30px;
+    color: black;
+    margin-left: 10px;
+}
+.setting-item button {
+    padding: 10px;
+    margin: 20px 60px;
+    font-family: 'Bebas Neue', sans-serif;
+    border: none;
+    font-size: 25px;
+    color: black;
+    background-color: #e4e4e4;
+    cursor: pointer;
+}
+.setting-item button:hover {
+    background-color: #a3a3a3;
+    border-radius: 4px;
+}
+</style>
