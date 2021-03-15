e929c47978813bb658dfdca0b7b898fa90250b8d
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-03-15 13:14:47 -0400

    js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 
 
+
+const path = require('path');
 const express = require('express');
+
+const usersCtrl = require('./controllers/users');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
-    .get('/', (req, res) => {
-    res.send('Hello World!')
-    })
+    .use(express.static('./docs'))
+
+
+    .use('/users', usersCtrl)
+
+        // last item
+    .get('*', (req, res) => {
+      res.sendFile( path.join(__dirname, '../docs/index.html' ) );
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
