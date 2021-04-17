0eeb9316190fdbae6bc20750386df4de9768631a
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-17 19:14:36 -0400

    using dotenv

--- client/src/models/myFetch.js
+++ client/src/models/myFetch.js
@@ -1,9 +1,9 @@
 
 
-export const API_ROOT = "http://localhost:3000/";
+export const API_ROOT = process.env.VUE_APP_API_ROOT;;
 
 export function api(url){
 
     return fetch(API_ROOT + url).then(x=> x.json());
-
+    
 }
