5a9a03cf142901169d4baee82913932ee48afb99
Jasmine Ennabe <ennabej1@hawkmail.newpaltz.edu>
2021-04-27 10:28:32 -0400

    update index router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,16 +3,20 @@ import VueRouter from 'vue-router'
 import Feed from '@/views/Feed.vue'
 import MyWall from '@/views/MyWall.vue'
 import Login from '@/views/Login.vue'
-import Users from '@/views/Users'
+// import Users from '@/views/Users'
 import Session from '@/models/Session'
+import Home from '@/views/Home'
+import UserProfile from '@/views/UserProfile'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'Feed', component: Feed },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/feed', name: 'Feed', component: Feed, props: true },
   { path: '/mywall', name: 'My Wall', component: MyWall },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/users', name: 'Users', component: Users },
+  // { path: '/users', name: 'Users', component: Users },
+  { path: '/user/:handle', name: 'UserProfile', component: UserProfile, props: true },
   { path: '/createworkout', name: 'Create Workout', component: () => import(/* webpackChunkName: "about" */ '../views/CreateWorkout.vue')},
   { path: '/myroutines', name: 'My Routines', component: () => import(/* webpackChunkName: "about" */ '../views/MyRoutines.vue')},
   { path: '/settings', name: 'Settings', component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue')},
@@ -32,6 +36,9 @@ router.beforeEach( (to, from, next) =>{
     if(to.path == '/mywall' && !Session.user) {
       Session.nextRoute = to;
       next('/login') 
+    } else if (to.path == '/feed' && !Session.user) {
+      Session.nextRoute = to;
+      next('/login')
     } else {
       next();
     }
